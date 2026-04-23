f7c431f9d5cc80ac0dce1c54b198ad1b12cd8243
Paweł Harbuz <pharbuz@yourcompass.ca>
2026-04-23 13:10:00 +0400

    Add AppEngineRegistryService and related tests for app management

--- dynatrace/platform/schemas.py
+++ dynatrace/platform/schemas.py
@@ -1,6 +1,7 @@
 """High-level platform API aggregators."""
 
 from dynatrace.http_client import HttpClient
+from dynatrace.platform.appengine.registry import AppEngineRegistryService
 from dynatrace.platform.davis.analyzers import AnalyzerService
 from dynatrace.platform.davis_copilot.copilot_service import CopilotService
 from dynatrace.platform.grail_dql_query.query_assistance import (
@@ -11,6 +12,11 @@ from dynatrace.platform.grail_dql_query.query_execution import QueryExecutionSer
 
 class PlatformAPI:
     def __init__(self, http_client: HttpClient) -> None:
+        # AppEngine platform APIs.
+        self.appengine_registry: AppEngineRegistryService = AppEngineRegistryService(
+            http_client
+        )
+
         # Davis platform APIs.
         self.davis_analyzers: AnalyzerService = AnalyzerService(http_client)
         self.davis_copilot: CopilotService = CopilotService(http_client)
